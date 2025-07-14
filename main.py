0e45c72c883f093d51b79fcb79924b3d023d5058
osama16118686 <beverly.b.c.d.ltd@gmail.com>
2025-07-15 02:55:51 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from bot.telegram_bot import run_bot
-from services.scheduler import start_scheduler
-
-if __name__ == "__main__":
-    print("🚀 Xentry Crypto Bot is starting...")
-    run_bot()
-    start_scheduler()
